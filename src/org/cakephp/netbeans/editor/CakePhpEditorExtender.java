3b99d4ca1c156267d9ee6fc052152954e9226557
evilbloodydemon <evilbloodydemon@gmail.com>
2010-07-29 15:52:32 +0400

    view vars extraction from Controller::set

--- src/org/cakephp/netbeans/editor/CakePhpEditorExtender.java
+++ src/org/cakephp/netbeans/editor/CakePhpEditorExtender.java
@@ -7,14 +7,33 @@ package org.cakephp.netbeans.editor;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.cakephp.netbeans.util.CakePhpUtils;
+import org.netbeans.modules.csl.spi.ParserResult;
+import org.netbeans.modules.parsing.api.ParserManager;
+import org.netbeans.modules.parsing.api.ResultIterator;
+import org.netbeans.modules.parsing.api.Source;
+import org.netbeans.modules.parsing.api.UserTask;
 import org.netbeans.modules.php.api.editor.PhpBaseElement;
 import org.netbeans.modules.php.api.editor.PhpClass;
 import org.netbeans.modules.php.api.editor.PhpVariable;
 import org.netbeans.modules.php.spi.editor.EditorExtender;
+import org.netbeans.modules.php.editor.parser.astnodes.visitors.DefaultVisitor;
+import org.netbeans.modules.parsing.spi.ParseException;
+import org.netbeans.modules.php.editor.CodeUtils;
+import org.netbeans.modules.php.editor.parser.PHPParseResult;
+import org.netbeans.modules.php.editor.parser.api.Utils;
+import org.netbeans.modules.php.editor.parser.astnodes.ClassDeclaration;
+import org.netbeans.modules.php.editor.parser.astnodes.Expression;
+import org.netbeans.modules.php.editor.parser.astnodes.FunctionInvocation;
+import org.netbeans.modules.php.editor.parser.astnodes.MethodDeclaration;
+import org.netbeans.modules.php.editor.parser.astnodes.MethodInvocation;
+import org.netbeans.modules.php.editor.parser.astnodes.Scalar;
 import org.openide.filesystems.FileObject;
 
 /**
@@ -30,8 +49,99 @@ public class CakePhpEditorExtender extends EditorExtender {
     public List<PhpBaseElement> getElementsForCodeCompletion(FileObject fo) {
         if (CakePhpUtils.isView(fo)) {
             List<PhpBaseElement> elements = new LinkedList<PhpBaseElement>(ELEMENTS);
+            elements.addAll(parseAction(fo));
             return elements;
         }
         return Collections.emptyList();
+    }
+
+    private Set<PhpVariable> parseAction(final FileObject view) {
+        final FileObject controller = CakePhpUtils.getController(view);
+        if (controller == null) {
+            return Collections.emptySet();
+        }
+        final Set<PhpVariable> phpVariables = new HashSet<PhpVariable>();
+        try {
+            ParserManager.parse(Collections.singleton(Source.create(controller)), new UserTask() {
+                @Override
+                public void run(ResultIterator resultIterator) throws Exception {
+                    ParserResult parseResult = (ParserResult) resultIterator.getParserResult();
+                    final CakePhpControllerVisitor controllerVisitor = new CakePhpControllerVisitor(view, (PHPParseResult) parseResult);
+                    controllerVisitor.scan(Utils.getRoot(parseResult));
+                    phpVariables.addAll(controllerVisitor.getPhpVariables());
+                }
+            });
+        } catch (ParseException ex) {
+            LOGGER.log(Level.WARNING, null, ex);
+        }
+
+        return phpVariables;
+    }
+
+    private static final class CakePhpControllerVisitor extends DefaultVisitor {
+        private final Set<PhpVariable> fields = new HashSet<PhpVariable>();
+
+        private String className = null;
+        private String methodName = null;
+        private String viewName = null;
+        
+        public CakePhpControllerVisitor(FileObject view, PHPParseResult actionParseResult) {
+            viewName = CakePhpUtils.getActionName(view);
+        }
+
+        public Set<PhpVariable> getPhpVariables() {
+            Set<PhpVariable> phpVariables = new HashSet<PhpVariable>();
+            synchronized (fields) {
+                phpVariables.addAll(fields);
+            }
+            return phpVariables;
+        }
+
+        @Override
+        public void visit(ClassDeclaration node) {
+            className = CodeUtils.extractClassName(node);
+            super.visit(node);
+        }
+
+        @Override
+        public void visit(MethodDeclaration node) {
+            methodName = CodeUtils.extractMethodName(node);
+            super.visit(node);
+        }
+
+        @Override
+        public void visit(MethodInvocation node) {
+            super.visit(node);
+            
+            FunctionInvocation fi = node.getMethod();
+            String invokedMethodName = CodeUtils.extractFunctionName(fi);
+
+            List<Expression> params = fi.getParameters();
+            Expression e = null;
+
+            if(!params.isEmpty()) {
+                e = params.get(0);
+            }
+
+            String viewVarName = "";
+            if(e instanceof Scalar) {
+                Scalar s = (Scalar)e;
+                if(s.getScalarType() == Scalar.Type.STRING) {
+                    viewVarName = s.getStringValue();
+                    if(!viewVarName.isEmpty()) {
+                        viewVarName = viewVarName.substring(1, viewVarName.length() - 1).trim();
+                    }
+                }
+            }
+
+            if(methodName.equals(viewName)
+                    && invokedMethodName.equals("set")
+                    && CakePhpUtils.isControllerName(className)
+                    && !viewVarName.isEmpty()) {
+                synchronized (fields) {
+                    fields.add(new PhpVariable("$" + viewVarName, new PhpClass("HtmlHelper", "HtmlHelper")));
+                }
+            }
+        }
     }
 }
