64ed031ab73ebb0a81bc3cb51c0e8879751be9aa
junichi11 <junichi0206@gmail.com>
2011-09-21 16:34:31 +0900

    View Path Toolbar added.

---
+++ src/org/cakephp/netbeans/ui/actions/ViewPathAction.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cakephp.netbeans.ui.actions;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import org.cakephp.netbeans.ui.ViewPathPanel;
+import org.openide.awt.ActionRegistration;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionID;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.actions.Presenter;
+
+@ActionID(category = "View",
+id = "org.cakephp.netbeans.ui.actions.ViewPathAction")
+@ActionRegistration(displayName = "#CTL_ViewPathAction")
+@ActionReferences({
+	@ActionReference(path = "Toolbars/View", position = 0)
+})
+@Messages("CTL_ViewPathAction=View Path")
+public final class ViewPathAction extends AbstractAction implements Presenter.Toolbar {
+	
+	public Component getToolbarPresenter(){
+		return new ViewPathPanel();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO implement action body
+	}
+	
+}
