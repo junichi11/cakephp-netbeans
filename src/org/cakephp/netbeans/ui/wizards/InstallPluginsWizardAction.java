1a02bdb76b5866236fe676ecf3234031783aca43
junichi11 <junichi0206@gmail.com>
2012-07-20 05:43:28 +0900

    format files

--- src/org/cakephp/netbeans/ui/wizards/InstallPluginsWizardAction.java
+++ src/org/cakephp/netbeans/ui/wizards/InstallPluginsWizardAction.java
@@ -28,87 +28,88 @@ import org.openide.util.NbBundle;
 
 // An example action demonstrating how the wizard could be called from within
 // your code. You can move the code below wherever you need, or register an action:
-@ActionID(category="PHP", id="org.cakephp.netbeans.ui.wizards.InstallPluginsWizardAction")
-@ActionRegistration(displayName="Install Plugins")
+@ActionID(category = "PHP", id = "org.cakephp.netbeans.ui.wizards.InstallPluginsWizardAction")
+@ActionRegistration(displayName = "Install Plugins")
 // @ActionReference(path="Menu/Tools", position=...)
 public final class InstallPluginsWizardAction extends BaseAction implements ActionListener {
-        private static final long serialVersionUID = 4405963698419409213L;
-        private static InstallPluginsWizardAction INSTANCE = new InstallPluginsWizardAction();
-        
-        private InstallPluginsWizardAction(){
-                
-        }
-        
-        public static InstallPluginsWizardAction getInstance(){
-                return INSTANCE;
-        }
 
-        @Override
-        protected String getFullName() {
-	        return NbBundle.getMessage(ClearCacheAction.class, "LBL_CakePhpAction", getPureName());
-        }
+    private static final long serialVersionUID = 4405963698419409213L;
+    private static InstallPluginsWizardAction INSTANCE = new InstallPluginsWizardAction();
 
-        @Override
-        protected String getPureName() {
-	        return NbBundle.getMessage(InstallPluginsWizardAction.class, "LBL_InstallPlugin");
-        }
+    private InstallPluginsWizardAction() {
+    }
 
-        @Override
-        protected void actionPerformed(PhpModule pm) {
-                List<WizardDescriptor.Panel<WizardDescriptor>> panels = new ArrayList<WizardDescriptor.Panel<WizardDescriptor>>();
-                panels.add(new InstallPluginsWizardPanel());
-                String[] steps = new String[panels.size()];
-                for (int i = 0; i < panels.size(); i++) {
-                        Component c = panels.get(i).getComponent();
-                        // Default step name to component name of panel.
-                        steps[i] = c.getName();
-                        if (c instanceof JComponent) { // assume Swing components
-                                JComponent jc = (JComponent) c;
-                                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX, i);
-                                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DATA, steps);
-                                jc.putClientProperty(WizardDescriptor.PROP_AUTO_WIZARD_STYLE, true);
-                                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DISPLAYED, true);
-                                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_NUMBERED, true);
-                        }
+    public static InstallPluginsWizardAction getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    protected String getFullName() {
+        return NbBundle.getMessage(ClearCacheAction.class, "LBL_CakePhpAction", getPureName());
+    }
+
+    @Override
+    protected String getPureName() {
+        return NbBundle.getMessage(InstallPluginsWizardAction.class, "LBL_InstallPlugin");
+    }
+
+    @Override
+    protected void actionPerformed(PhpModule pm) {
+        List<WizardDescriptor.Panel<WizardDescriptor>> panels = new ArrayList<WizardDescriptor.Panel<WizardDescriptor>>();
+        panels.add(new InstallPluginsWizardPanel());
+        String[] steps = new String[panels.size()];
+        for (int i = 0; i < panels.size(); i++) {
+            Component c = panels.get(i).getComponent();
+            // Default step name to component name of panel.
+            steps[i] = c.getName();
+            if (c instanceof JComponent) { // assume Swing components
+                JComponent jc = (JComponent) c;
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX, i);
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DATA, steps);
+                jc.putClientProperty(WizardDescriptor.PROP_AUTO_WIZARD_STYLE, true);
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DISPLAYED, true);
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_NUMBERED, true);
+            }
+        }
+        WizardDescriptor wiz = new WizardDescriptor(new WizardDescriptor.ArrayIterator<WizardDescriptor>(panels));
+        // {0} will be replaced by WizardDesriptor.Panel.getComponent().getName()
+        wiz.setTitleFormat(new MessageFormat("{0}"));
+        wiz.setTitle("CakePHP Install Plugins");
+        if (DialogDisplayer.getDefault().notify(wiz) == WizardDescriptor.FINISH_OPTION) {
+            Panel<WizardDescriptor> panel = (InstallPluginsWizardPanel) panels.get(0);
+            InstallPluginsVisualPanel component = (InstallPluginsVisualPanel) panel.getComponent();
+            String installPath = component.getInstallPathTextField();
+            FileObject fo = CakePhpFrameworkProvider.getCakePhpDirectory(pm).getFileObject(installPath);
+            NotifyDescriptor descriptor = null;
+            if (fo == null) {
+                descriptor = new NotifyDescriptor.Message(installPath + " dosen't exist.", NotifyDescriptor.ERROR_MESSAGE);
+                DialogDisplayer.getDefault().notifyLater(descriptor);
+                return;
+            }
+            List<CakePhpPlugin> plugins = component.getCakePhpPluginList();
+            // error list
+            StringBuilder errors = new StringBuilder();
+
+            // unzip
+            for (CakePhpPlugin plugin : plugins) {
+
+                if (plugin.isInstall()) {
+                    CakeZip zip = new CakeZip(plugin.getName());
+                    try {
+                        zip.unzip(plugin.getUrl(), fo);
+                    } catch (IOException ex) {
+                        errors.append(plugin.getName()).append("\n");
+                    }
                 }
-                WizardDescriptor wiz = new WizardDescriptor(new WizardDescriptor.ArrayIterator<WizardDescriptor>(panels));
-                // {0} will be replaced by WizardDesriptor.Panel.getComponent().getName()
-                wiz.setTitleFormat(new MessageFormat("{0}"));
-                wiz.setTitle("CakePHP Install Plugins");
-                if (DialogDisplayer.getDefault().notify(wiz) == WizardDescriptor.FINISH_OPTION) {
-                        Panel<WizardDescriptor> panel = (InstallPluginsWizardPanel)panels.get(0);
-                        InstallPluginsVisualPanel component = (InstallPluginsVisualPanel) panel.getComponent();
-                        String installPath = component.getInstallPathTextField();
-                        FileObject fo = CakePhpFrameworkProvider.getCakePhpDirectory(pm).getFileObject(installPath);
-                        NotifyDescriptor descriptor = null;
-                        if(fo == null){
-                                descriptor = new NotifyDescriptor.Message(installPath + " dosen't exist.", NotifyDescriptor.ERROR_MESSAGE);
-                                DialogDisplayer.getDefault().notifyLater(descriptor);
-                                return;
-                        }
-                        List<CakePhpPlugin> plugins = component.getCakePhpPluginList();
-                        // error list
-                        StringBuilder errors = new StringBuilder();
-                        
-                        // unzip
-                        for(CakePhpPlugin plugin : plugins){
-                                if(plugin.isInstall()){
-                                        CakeZip zip = new CakeZip(plugin.getName());
-                                        try {
-                                                zip.unzip(plugin.getUrl(), fo);
-                                        } catch (IOException ex) {
-                                                errors.append(plugin.getName()).append("\n");
-                                        }
-                                }
-                        }
-                        
-                        String errorMessage = errors.toString();
-                        if(!errorMessage.isEmpty()){ //display error dialog
-                                descriptor = new NotifyDescriptor.Message("Please confirm the URL.\n" + errorMessage, NotifyDescriptor.ERROR_MESSAGE);
-                        }else{ // display complete dialog
-                                descriptor = new NotifyDescriptor.Message("Install Complete!", NotifyDescriptor.INFORMATION_MESSAGE);
-                        }
-                        DialogDisplayer.getDefault().notifyLater(descriptor);
-                }
+            }
+
+            String errorMessage = errors.toString();
+            if (!errorMessage.isEmpty()) { //display error dialog
+                descriptor = new NotifyDescriptor.Message("Please confirm the URL.\n" + errorMessage, NotifyDescriptor.ERROR_MESSAGE);
+            } else { // display complete dialog
+                descriptor = new NotifyDescriptor.Message("Install Complete!", NotifyDescriptor.INFORMATION_MESSAGE);
+            }
+            DialogDisplayer.getDefault().notifyLater(descriptor);
         }
+    }
 }
