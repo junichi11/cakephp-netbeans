88c64f85b9049e4427c9f1ca3eadb5defb4e2903
junichi11 <junichi0206@gmail.com>
2013-03-05 13:04:51 +0900

    Format EmbeddingProvider

--- src/org/cakephp/netbeans/editor/embedding/CakePhpEmbeddingProvider.java
+++ src/org/cakephp/netbeans/editor/embedding/CakePhpEmbeddingProvider.java
@@ -58,59 +58,60 @@ import org.netbeans.modules.php.editor.lexer.PHPTokenId;
  *
  * @author junichi11
  */
-public class CakePhpEmbeddingProvider extends EmbeddingProvider{
+public class CakePhpEmbeddingProvider extends EmbeddingProvider {
+
+    @Override
+    public List<Embedding> getEmbeddings(Snapshot snpsht) {
+        TokenHierarchy<?> th = snpsht.getTokenHierarchy();
+        TokenSequence<PHPTokenId> sequence = th.tokenSequence(PHPTokenId.language());
+
+        if (sequence == null) {
+            return Collections.emptyList();
+        }
+        sequence.moveStart();
+        List<Embedding> embeddings = new ArrayList<Embedding>();
+        boolean scriptFlg = false;
+        while (sequence.moveNext()) {
+            Token<? extends TokenId> t = sequence.token();
+            TokenId id = t.id();
+            String s = id.primaryCategory();
+            if (scriptFlg) {
+                if (id == PHPTokenId.T_INLINE_HTML) {
+                    embeddings.add(snpsht.create(sequence.offset(), t.length(), "text/javascript")); // NOI18N
+                    scriptFlg = false;
+                }
+            }
+
+            if (id == PHPTokenId.PHP_STRING) {
+                if (TokenUtilities.startsWith(t.text(), "scriptStart")) { // NOI18N
+                    scriptFlg = true;
+                }
+            }
+        }
+
+        if (embeddings.isEmpty()) {
+            return Collections.emptyList();
+        } else {
+            return Collections.singletonList(Embedding.create(embeddings));
+        }
+    }
+
+    @Override
+    public int getPriority() {
+        return 200;
+    }
+
+    @Override
+    public void cancel() {
+        // do nothing
+    }
+
+    @MimeRegistration(service = TaskFactory.class, mimeType = "text/x-php5") // NOI18N
+    public static final class Factory extends TaskFactory {
 
         @Override
-        public List<Embedding> getEmbeddings(Snapshot snpsht) {
-                TokenHierarchy<?> th = snpsht.getTokenHierarchy();
-                TokenSequence<PHPTokenId> sequence = th.tokenSequence(PHPTokenId.language());
-
-                if(sequence == null){
-                        return Collections.emptyList();
-                }
-                sequence.moveStart();
-                List<Embedding> embeddings = new ArrayList<Embedding>();
-                boolean scriptFlg = false;
-                while (sequence.moveNext()){
-                        Token<? extends TokenId> t = sequence.token();
-                        TokenId id = t.id();
-                        String s = id.primaryCategory();
-                        if(scriptFlg){
-                                if(id == PHPTokenId.T_INLINE_HTML){
-                                        embeddings.add(snpsht.create(sequence.offset(), t.length(), "text/javascript")); // NOI18N
-                                        scriptFlg = false;
-                                }
-                        }
-
-                        if(id == PHPTokenId.PHP_STRING){
-                                if(TokenUtilities.startsWith(t.text(), "scriptStart")){ // NOI18N
-                                        scriptFlg = true;
-                                }
-                        }
-                }
-
-                if(embeddings.isEmpty()){
-                        return Collections.emptyList();
-                }else{
-                        return Collections.singletonList(Embedding.create(embeddings));
-                }
+        public Collection<SchedulerTask> create(final Snapshot snapshot) {
+            return Collections.<SchedulerTask>singletonList(new CakePhpEmbeddingProvider());
         }
-
-        @Override
-        public int getPriority() {
-                return 200;
-        }
-
-        @Override
-        public void cancel() {
-                // do nothing
-        }
-
-        @MimeRegistration(service = TaskFactory.class, mimeType = "text/x-php5") // NOI18N
-        public static final class Factory extends TaskFactory {
-                @Override
-                public Collection<SchedulerTask> create(final Snapshot snapshot) {
-                        return Collections.<SchedulerTask>singletonList(new CakePhpEmbeddingProvider());
-                }
-        }
+    }
 }
