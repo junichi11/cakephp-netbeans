0a8ab03df006d9666f919f5b71b08e7fd8393002
junichi11 <junichi0206@gmail.com>
2013-03-20 16:19:30 +0900

    Format

--- src/org/cakephp/netbeans/options/CakePhpOptions.java
+++ src/org/cakephp/netbeans/options/CakePhpOptions.java
@@ -53,57 +53,57 @@ import org.openide.util.NbPreferences;
  * @author junichi11
  */
 public class CakePhpOptions {
-        private static final int NAME = 0;
-        private static final int URL = 1;
-        private static final String PLUGINS = "plugins"; // NOI18N
-        private static CakePhpOptions INSTANCE = new CakePhpOptions();
 
-        private CakePhpOptions(){
+    private static final int NAME = 0;
+    private static final int URL = 1;
+    private static final String PLUGINS = "plugins"; // NOI18N
+    private static CakePhpOptions INSTANCE = new CakePhpOptions();
 
+    private CakePhpOptions() {
+    }
+
+    public static CakePhpOptions getInstance() {
+        return INSTANCE;
+    }
+
+    public List<CakePhpPlugin> getPlugins() {
+        ArrayList<CakePhpPlugin> plugins = new ArrayList<CakePhpPlugin>();
+        Preferences p = getPreferences().node(PLUGINS);
+        String s = "";
+        if (p != null) {
+            s = p.get(PLUGINS, ""); // NOI18N
+        }
+        if (s.isEmpty()) {
+            s = NbBundle.getMessage(CakePhpOptions.class, "CakePhpOptions.defaultPlugins"); // NOI18N
         }
 
-        public static CakePhpOptions getInstance(){
-                return INSTANCE;
+        String[] rows = s.split(";"); // NOI18N
+        Arrays.sort(rows);
+        for (String row : rows) {
+            String[] cells = row.split(","); // NOI18N
+            if (cells.length == 2) {
+                plugins.add(new CakePhpPlugin(cells[NAME], cells[URL]));
+            }
         }
+        return plugins;
+    }
 
-        public List<CakePhpPlugin> getPlugins(){
-                ArrayList<CakePhpPlugin> plugins = new ArrayList<CakePhpPlugin>();
-                Preferences p = getPreferences().node(PLUGINS);
-                String s = "";
-                if(p != null){
-                        s = p.get(PLUGINS, "");
-                }
-                if(s.isEmpty()){
-                        s = NbBundle.getMessage(CakePhpOptions.class, "CakePhpOptions.defaultPlugins"); // NOI18N
-                }
-
-                String[] rows = s.split(";");
-                Arrays.sort(rows);
-                for (String row : rows) {
-                        String[] cells = row.split(",");
-                        if (cells.length == 2) {
-                                plugins.add(new CakePhpPlugin(cells[NAME], cells[URL]));
-                        }
-                }
-                return plugins;
+    public void setPlugins(List<CakePhpPlugin> plugins) {
+        Preferences p = getPreferences().node(PLUGINS);
+        String lists = ""; // NOI18N
+        boolean first = true;
+        for (CakePhpPlugin plugin : plugins) {
+            if (first) {
+                first = false;
+            } else {
+                lists += ";"; // NOI18N
+            }
+            lists += plugin.getName() + "," + plugin.getUrl(); // NOI18N
         }
+        p.put(PLUGINS, lists);
+    }
 
-        public void setPlugins(List<CakePhpPlugin> plugins){
-                Preferences p = getPreferences().node(PLUGINS);
-                String lists = "";
-                boolean first = true;
-                for(CakePhpPlugin plugin: plugins){
-                        if(first){
-                                first = false;
-                        }else{
-                                lists += ";";
-                        }
-                        lists += plugin.getName() + "," + plugin.getUrl();
-                }
-                p.put(PLUGINS, lists);
-        }
-
-        public Preferences getPreferences(){
-                return NbPreferences.forModule(CakePhpOptions.class).node(PLUGINS);
-        }
+    public Preferences getPreferences() {
+        return NbPreferences.forModule(CakePhpOptions.class).node(PLUGINS);
+    }
 }
