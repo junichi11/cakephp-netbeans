33fe1c3133c31bc449f561f1b73bab50e3ee70ec
junichi11 <junichi0206@gmail.com>
2013-06-13 17:29:39 +0900

    Fix CakePhp3ModuleImpl

--- src/org/cakephp/netbeans/module/CakePhp3ModuleImpl.java
+++ src/org/cakephp/netbeans/module/CakePhp3ModuleImpl.java
@@ -41,10 +41,36 @@
  */
 package org.cakephp.netbeans.module;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static org.cakephp.netbeans.module.CakePhpModule.DIR_TYPE.APP;
+import static org.cakephp.netbeans.module.CakePhpModule.DIR_TYPE.APP_LIB;
+import static org.cakephp.netbeans.module.CakePhpModule.DIR_TYPE.APP_PLUGIN;
+import static org.cakephp.netbeans.module.CakePhpModule.DIR_TYPE.APP_VENDOR;
+import static org.cakephp.netbeans.module.CakePhpModule.DIR_TYPE.CORE;
+import static org.cakephp.netbeans.module.CakePhpModule.DIR_TYPE.PLUGIN;
+import static org.cakephp.netbeans.module.CakePhpModule.DIR_TYPE.VENDOR;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.BEHAVIOR;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.COMPONENT;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.CONFIG;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.CONSOLE;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.CONTROLLER;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.HELPER;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.MODEL;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.NONE;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.TEST;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.VIEW;
+import static org.cakephp.netbeans.module.CakePhpModule.FILE_TYPE.WEBROOT;
+import org.cakephp.netbeans.preferences.CakePreferences;
 import org.netbeans.modules.php.api.phpmodule.PhpModule;
 import org.openide.filesystems.FileObject;
 
 public class CakePhp3ModuleImpl extends CakePhp2ModuleImpl {
+
+    private static final Logger LOGGER = Logger.getLogger(CakePhp3ModuleImpl.class.getName());
+    private static final String DIR_COMPONENT = "Component"; // NOI18N
+    private static final String DIR_HELPER = "Helper"; // NOI18N
+    private static final String DIR_BEHAVIOR = "Behavior"; // NOI18N
 
     public CakePhp3ModuleImpl(PhpModule phpModule) {
         super(phpModule);
@@ -57,5 +83,137 @@ public class CakePhp3ModuleImpl extends CakePhp2ModuleImpl {
             return configDirectory.getFileObject("app.php"); // NOI18N
         }
         return null;
+    }
+
+    @Override
+    public FileObject getDirectory(CakePhpModule.DIR_TYPE type, CakePhpModule.FILE_TYPE fileType, String pluginName) {
+        if (type == null) {
+            return null;
+        }
+        if (fileType == null && pluginName == null) {
+            return getDirectory(type);
+        }
+        if (pluginName != null) {
+            switch (type) {
+                case APP: // no break
+                case APP_LIB: // no break
+                case APP_VENDOR: // no break
+                case CORE: // no break
+                case VENDOR: // no break
+                case PLUGIN:
+                    return null;
+                default:
+                    break;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        switch (type) {
+            case APP_LIB: // no break
+            case APP_VENDOR:
+                if (fileType == null || fileType == CakePhpModule.FILE_TYPE.NONE) {
+                    return getDirectory(type);
+                } else {
+                    return null;
+                }
+            case APP_PLUGIN: // no break
+                if (pluginName == null || pluginName.isEmpty()) {
+                    return null;
+                }
+                if (fileType == null) {
+                    return getDirectory(type).getFileObject(pluginName);
+                }
+                sb.append(pluginName);
+                sb.append("/"); // NOI18N
+            case CORE: // no break
+            case APP:
+                switch (fileType) {
+                    case CONTROLLER: // no break
+                    case VIEW: // no break
+                    case MODEL:
+                        sb.append(fileType.toString());
+                        break;
+                    case COMPONENT:
+                        sb.append("Controller/"); // NOI18N
+                        sb.append(DIR_COMPONENT);
+                        break;
+                    case HELPER:
+                        sb.append("View/"); // NOI18N
+                        sb.append(DIR_HELPER);
+                        break;
+                    case BEHAVIOR:
+                        sb.append("Model/"); // NOI18N
+                        sb.append(DIR_BEHAVIOR);
+                        break;
+                    case TEST:
+                        sb.append("Test"); // NOI18N
+                        break;
+                    case CONFIG:
+                        sb.append("Config"); // NOI18N
+                        break;
+                    case CONSOLE:
+                        sb.append("Console"); // NOI18N
+                        break;
+                    case WEBROOT:
+                        if (type == CakePhpModule.DIR_TYPE.CORE) {
+                            return null;
+                        }
+                        sb.append("webroot"); // NOI18N
+                        break;
+                    case NONE:
+                        if (type == CakePhpModule.DIR_TYPE.APP_PLUGIN) {
+                            return getDirectory(type).getFileObject(pluginName);
+                        }
+                        return getDirectory(type);
+                    default:
+                        return null;
+                }
+                break;
+            default:
+                return null;
+        }
+        FileObject directory = getDirectory(type);
+        if (directory == null) {
+            return null;
+        }
+        return directory.getFileObject(sb.toString());
+    }
+
+    @Override
+    public FileObject getDirectory(CakePhpModule.DIR_TYPE type) {
+        if (type == null) {
+            return null;
+        }
+        FileObject sourceDirectory = getCakePhpDirectory();
+        if (sourceDirectory == null) {
+            LOGGER.log(Level.WARNING, "Not found source directory");
+            return null;
+        }
+        String path = ""; // NOI18N
+        String app = CakePreferences.getAppName(phpModule);
+        switch (type) {
+            case APP:
+                path = app;
+                break;
+            case APP_LIB:
+                path = app + "/Lib"; // NOI18N
+                break;
+            case APP_PLUGIN:
+                path = app + "/Plugin"; // NOI18N
+                break;
+            case APP_VENDOR:
+                path = app + "/vendor"; // NOI18N
+                break;
+            case CORE:
+                path = "lib/Cake"; // NOI18N
+                break;
+            case PLUGIN: // no break
+            case VENDOR:
+                return null;
+            default:
+                throw new AssertionError();
+        }
+
+        return sourceDirectory.getFileObject(path);
     }
 }
